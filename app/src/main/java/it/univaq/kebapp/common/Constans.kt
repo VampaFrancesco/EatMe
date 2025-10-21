7892f0f0de2b0d423d511e062a64d22ae930d534
Francesco <77879644+VampaFrancesco@users.noreply.github.com>
2025-10-22 00:50:58 +0200

    Add INTERNET permission and update BASE_URL
    Added the INTERNET permission to AndroidManifest.xml to allow network operations. Updated BASE_URL in Constans.kt from localhost to 10.0.2.2 for proper emulator access to the host machine.

--- app/src/main/java/it/univaq/kebapp/common/Constans.kt
+++ app/src/main/java/it/univaq/kebapp/common/Constans.kt
@@ -1,6 +1,6 @@
 package it.univaq.kebapp.common
 
-const val BASE_URL = "http://localhost:8080/"
+const val BASE_URL = "http://10.0.2.2:8080/"
 const val API_DATA = "kebabbari.json"
 
 //minuto 13:49
